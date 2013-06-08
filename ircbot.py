b010cbee21619f478af737a2179f0ab2fb9fb896
Ryan Pearl <rpearl@endofunctor.org>
2013-06-08 20:34:43 +0000

    refactor for an IRCBot class

---
+++ ircbot.py
@@ -0,0 +1,87 @@
+from tornado_irc import IRCConn
+import traceback
+
+class Command(object):
+    def __init__(self, predicate, f, docstring=None):
+        self.f = f
+        self.predicate = predicate
+        self.docstring = docstring
+
+    def __call__(self, bot, channel, user, message):
+        ret = self.predicate(bot, channel, user, message)
+        if ret:
+            resp = self.f(bot, channel, user, message)
+            if resp:
+                bot.reply(channel, user, resp)
+        return ret
+
+def response(trigger):
+    def _dec(f):
+        def pred(bot, channel, user, message):
+            return message.startswith(bot.nickname) and trigger in message
+        return Command(pred, f)
+    return _dec
+
+def command(trigger):
+    def _dec(f):
+        cmd = '%'+trigger
+        def wrapper(f):
+            def wrap(bot, channel, user, message):
+                return f(bot, channel, user, message[len(cmd):].strip())
+            return wrap
+        def pred(bot, channel, user, message):
+            return message.split()[0] == cmd
+
+        if f.__doc__:
+            doc = '%s: %s' % (cmd, f.__doc__)
+        else:
+            doc = '%s' % cmd
+
+        return Command(pred, wrapper(f), docstring=doc)
+    return _dec
+
+class IRCBot(IRCConn):
+    def __init__(self, nickname, io_loop=None, channels=[], owner="someone"):
+        super(IRCBot, self).__init__(nickname, io_loop)
+        self.channels = set(channels)
+        self.commands = set()
+        self.owner = owner
+        for name in dir(self):
+            f = getattr(self, name)
+            if isinstance(f, Command):
+                self.commands.add(f)
+
+    def on_connect(self):
+        print "connected"
+        for channel in self.channels:
+            self.join(channel)
+
+    def reply(self, channel, user, message):
+        out = "%s: %s" % (user, message)
+        self.chanmsg(channel, out)
+
+    def join(self, channel):
+        super(IRCBot, self).join(channel)
+        self.channels.add(channel)
+
+    @command(trigger='help')
+    def help(self, channel, user, message):
+        """Show this message"""
+        out = []
+        for command in self.commands:
+            if command.docstring:
+                out.append(command.docstring)
+        if len(out):
+            return "I know the following commands: %s" % ('. '.join(out))
+        elif len(self.commands):
+            return "I know some commands, but have no documentation. :-("
+        else:
+            return "I know no commands."
+
+    def on_chanmsg(self, channel, user, message):
+        try:
+            for command in self.commands:
+                if command(self, channel, user, message): break
+        except Exception, e:
+            self.reply(channel, user, 'an error occurred. Ping %s about it.' % (self.owner,))
+            print traceback.format_exc()
--- lunchbot.py
+++ lunchbot.py
@@ -1,4 +1,3 @@
-
 from collections import defaultdict
 from datetime import (
     datetime,
@@ -10,71 +9,18 @@ from random import choice
 import signal
 import time
 
+from ircbot import (
+    IRCBot,
+    response,
+    command,
+)
+
 import tornado.ioloop
-from tornado_irc import IRCConn
 
-class Command(object):
-    def __init__(self, predicate, f, docstring=None):
-        self.f = f
-        self.predicate = predicate
-        self.docstring = docstring
-
-    def __call__(self, bot, channel, user, message):
-        ret = self.predicate(bot, channel, user, message)
-        if ret:
-            resp = self.f(bot, channel, user, message)
-            if resp:
-                bot.reply(channel, user, resp)
-        return ret
-
-def response(trigger):
-    def _dec(f):
-        def pred(bot, channel, user, message):
-            return message.startswith(bot.nickname) and trigger in message
-        return Command(pred, f)
-    return _dec
-
-def command(trigger):
-    def _dec(f):
-        cmd = '%'+trigger
-        def wrapper(f):
-            def wrap(bot, channel, user, message):
-                return f(bot, channel, user, message[len(cmd):].strip())
-            return wrap
-        def pred(bot, channel, user, message):
-            return message.split()[0] == cmd
-
-        if f.__doc__:
-            doc = '%s: %s' % (cmd, f.__doc__)
-        else:
-            doc = '%s' % cmd
-
-        return Command(pred, wrapper(f), docstring=doc)
-    return _dec
-
-class LunchBot(IRCConn):
-    def __init__(self, nickname, io_loop=None, channels=[]):
-        self.channels = set(channels)
-        super(LunchBot, self).__init__(nickname, io_loop)
+class LunchBot(IRCBot):
+    def __init__(self, *args, **kwargs):
+        super(LunchBot, self).__init__(*args, **kwargs)
         self.timers = defaultdict(dict)
-        self.commands = set()
-        for name in dir(self):
-            f = getattr(self, name)
-            if isinstance(f, Command):
-                self.commands.add(f)
-
-    def on_connect(self):
-        print "connected"
-        for channel in self.channels:
-            self.join(channel)
-
-    def reply(self, channel, user, message):
-        out = "%s: %s" % (user, message)
-        self.chanmsg(channel, out)
-
-    def join(self, channel):
-        super(LunchBot, self).join(channel)
-        self.channels.add(channel)
 
     @response(trigger='botsnack')
     def botsnack(self, channel, user, message):
@@ -132,29 +78,9 @@ class LunchBot(IRCConn):
         del self.timers[user][message]
         return "Okay, removing timer for '%s'." % (message,)
 
-    @command(trigger='help')
-    def help(self, channel, user, message):
-        """Show this message"""
-        out = []
-        for command in self.commands:
-            if command.docstring:
-                out.append(command.docstring)
-        if len(out):
-            return "I know the following commands: %s" % ('. '.join(out))
-        elif len(self.commands):
-            return "I know some commands, but have no documentation. :-("
-        else:
-            return "I know no commands."
-
-    def on_chanmsg(self, *args):
-        try:
-            for command in self.commands:
-                if command(self, *args): break
-        except Exception, e:
-            self.reply('an error occurred. Ping rpearl about it.')
 
 if __name__ == '__main__':
-    c = LunchBot('cslunchbot', channels=["#cslunch"])
+    c = LunchBot('cslunchbot', channels=["#cslunch"], owner='rpearl')
     c.connect('irc.freenode.net', 6667)
 
     signal.signal(signal.SIGINT, lambda *args: tornado.ioloop.IOLoop.instance().stop())
